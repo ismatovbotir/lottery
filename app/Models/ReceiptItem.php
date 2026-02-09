3e4d95aafbdad394226ab19cf6100ed834bf28ed
Botir <tiboruz@gmail.com>
2026-02-09 17:07:02 +0500

    sdfsdfasdf aregae g ger

--- app/Models/ReceiptItem.php
+++ app/Models/ReceiptItem.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ReceiptItem extends Model
 {
     use HasFactory;
+    public $guarded=[];
 }
--- app/Models/ReceiptPayment.php
+++ app/Models/ReceiptPayment.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ReceiptPayment extends Model
 {
     use HasFactory;
+    public $guarded=[];
 }
